99beceecc4400fbe9ec7e8034a1a317b641d47ff
Seah Ying Xiang <seahyx@gmail.com>
2018-07-04 15:56:07 +0800

    Done some stuff like fixing repo

--- submodule_update.bat
+++ submodule_update.bat
@@ -1,1 +1,3 @@
-git submodule foreach git pull origin master
+cd "D:\Users\seahy\Documents\GitHub\seahyx.github.io\"
+@RD /S /Q "floating-menu"
+git clone https://github.com/seahyx/floating-menu.git floating-menu
