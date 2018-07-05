1febee622c74e163dabf78f11c387a647046d32c
Seah Ying Xiang <seahyx@gmail.com>
2018-07-05 19:14:03 +0800

    reverted changes

--- submodule_update.bat
+++ submodule_update.bat
@@ -1,1 +1,3 @@
-git submodule update --init --recursive
+cd "D:\Users\seahy\Documents\GitHub\seahyx.github.io\"
+@RD /S /Q "floating-menu"
+git clone https://github.com/seahyx/floating-menu.git floating-menu
