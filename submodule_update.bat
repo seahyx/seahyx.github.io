ae9b694b7c9621c5d91c5f9c09c80616f60abf28
Seah Ying Xiang <seahyx@gmail.com>
2019-05-17 00:11:20 +0800

    Changed the look of the home page

--- submodule_update.bat
+++
@@ -1,4 +0,0 @@
-cd "D:\Users\seahy\Documents\GitHub\seahyx.github.io\"
-@RD /S /Q "floating-menu"
-git clone --depth=1 https://github.com/seahyx/floating-menu.git floating-menu
-@RD /S /Q "floating-menu\.git"
